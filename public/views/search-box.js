1b617d3dd8d445b29886ac833b126cc0831e8ee2
Jerome Dane <jerome.dane@gmail.com>
2015-06-29 23:49:37 -0700

    Search box date comments and date format fix

--- public/views/search-box.js
+++ public/views/search-box.js
@@ -4,24 +4,27 @@ define([
 	
 	function initDatePickers(view) {
 		
+		// get checkin/checkout fields
 		$checkinField = $('input[name="checkin"]', view.$el);
 		$checkoutField = $('input[name="checkout"]', view.$el);
 		
+		// initialize both fields the same way
 		_.each([$checkinField, $checkoutField], function($field) {
 			$field.datepicker({
 				minDate: 0,
 				numberOfMonths: 2,
-				slideshowAnim: "slideDown"
+				slideshowAnim: "slideDown",
+				dateFormat: "yy-mm-dd"
 			});	
-			$field.change(function() {
-				$field.datepicker( "option", "dateFormat", "yy-mm-dd");
-			});
 		});
 		
+		// checking field specific settings
 		$checkinField.datepicker("option", "onClose", function(selectedDate) {
 			$checkoutField.datepicker( "option", "minDate", selectedDate );
 		});
 		$checkinField.datepicker('setDate', 'now');
+		
+		// checkout field specific settings
 		$checkoutField.datepicker("option", "onClose", function(selectedDate) {
 			$checkinField.datepicker( "option", "maxDate", selectedDate );
 		});
