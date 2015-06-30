fb95cfd3a68ac86582e503fd20ddce861a0b9758
Jerome Dane <jerome.dane@gmail.com>
2015-06-29 22:48:19 -0700

    Search box and result skeletons

--- public/templates/search.tpl
+++ public/templates/search.tpl
@@ -1,1 +1,2 @@
-search template ...
+<div class="view view-search-box"></div>
+<div class="view view-search-results"></div>
--- public/views/search.js
+++ public/views/search.js
@@ -1,14 +1,34 @@
 define([
-	'text!templates/search.tpl'
-], function(templateHtml) {
+	'text!templates/search.tpl',
+	'views/search-box',
+	'views/search-results'
+], function(
+	templateHtml,
+	SearchBoxView,
+	SearchResultsView
+) {
+	
+	var renderSubView = com.jeromedane.Utils.renderSubView;
 	
 	var view = {
 		
 		template: _.template(templateHtml),
 		
+		initialize: function() {
+			
+			this.views = {
+				'search-box': new SearchBoxView(),
+				'search-results': new SearchResultsView()
+			};
+			
+		},
+		
 		render: function() {
 			
 			this.$el.html(this.template());
+			
+			renderSubView(this, 'search-box');
+			renderSubView(this, 'search-results');
 			
 		}
 	};
