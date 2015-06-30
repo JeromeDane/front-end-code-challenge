4992c11f1761955a321e6872a038504ba8313b79
Jerome Dane <jerome.dane@gmail.com>
2015-06-29 23:45:54 -0700

    Search box date field skeleton

--- public/views/search-box.js
+++ public/views/search-box.js
@@ -2,6 +2,34 @@ define([
 	'text!templates/search-box.tpl'
 ], function(templateHtml) {
 	
+	function initDatePickers(view) {
+		
+		$checkinField = $('input[name="checkin"]', view.$el);
+		$checkoutField = $('input[name="checkout"]', view.$el);
+		
+		_.each([$checkinField, $checkoutField], function($field) {
+			$field.datepicker({
+				minDate: 0,
+				numberOfMonths: 2,
+				slideshowAnim: "slideDown"
+			});	
+			$field.change(function() {
+				$field.datepicker( "option", "dateFormat", "yy-mm-dd");
+			});
+		});
+		
+		$checkinField.datepicker("option", "onClose", function(selectedDate) {
+			$checkoutField.datepicker( "option", "minDate", selectedDate );
+		});
+		$checkinField.datepicker('setDate', 'now');
+		$checkoutField.datepicker("option", "onClose", function(selectedDate) {
+			$checkinField.datepicker( "option", "maxDate", selectedDate );
+		});
+		$checkoutField.datepicker('setDate', "+1d");
+		
+		
+	}
+	
 	var view = {
 		
 		template: _.template(templateHtml),
@@ -10,6 +38,7 @@ define([
 			
 			this.$el.html(this.template());
 			
+			initDatePickers(this);
 		}
 	};
 	
