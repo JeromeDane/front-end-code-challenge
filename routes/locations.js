1755d06b9601b78cdaf3fee4a19a6835a909f6ca
Jerome Dane <jerome.dane@gmail.com>
2015-06-30 21:34:01 -0700

    Use US zipcode data rather than just 3 locations, and allow searching locations to retrieve a limited set

--- routes/locations.js
+++ routes/locations.js
@@ -4,7 +4,7 @@ var _          = require('lodash');
 var url        = require('url');
 var randomSeed = require('random-seed');
 
-var locations = require('../data/locations.json');
+var locations = require('../data/zipcodes.json');
 
 function cloneRequiredJson(obj) {
   return JSON.parse(JSON.stringify(obj));
@@ -51,6 +51,36 @@ function getHotels(location, req) {
 // Get locations
 router.get('/', function(req, res) {
   res.json(_.toArray(locations));
+});
+
+/**
+ *  Search locations
+ *  
+ *  Note: This is NOT optimal code given a large dataset. This 
+ *  query could take quite some time when using a flat
+ *  database structure like JSON files. I'm doing it this way
+ *  to add interest to the coding challenge without completely
+ *  changing the DB system. Normally I would use a DB that allows
+ *  for more optimal data retrieval using queries (e.g. sql, etc.)
+ */
+router.get('/search/:name', function(req, res) {
+  var searchStr = res.req.params.name;
+  // get only the locations that have the search string in the combined state, city, and postal code
+  var filteredLocations = _.filter(locations, function(location){
+	  var compositeLocationStr = (location.state + ' ' + location.city + ' ' + location.postal).toLowerCase();
+	  return compositeLocationStr.indexOf(searchStr.toLowerCase()) != -1;
+  });
+  // sample zipcode location data contains duplicate data, but we don't care for this coding challenge
+  if(searchStr.match(/^\d+$/)) {
+    // if the search only contains numers (is probably a sipcode) then remove duplicate zipcode entries
+	filteredLocations = _.indexBy(_.toArray(filteredLocations), 'postal');
+  } else {
+	// if search is probably not a zipcode, then remove duplicate city/state combinations
+	filteredLocations = _.indexBy(_.toArray(filteredLocations), function(location) {
+		return location.city + ' ' + location.state;
+	});
+  }
+  res.json(_.toArray(filteredLocations));
 });
 
 // Get a location by id
