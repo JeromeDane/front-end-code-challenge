17b624cf5c1aefa7322d2a6eef27dabdf9990b3e
unknown <DaneJ@DaneJ-8W.darden.virginia.edu>
2015-07-07 10:34:43 -0400

    Hotel star mouseover titles

--- public/views/hotel-preview.js
+++ public/views/hotel-preview.js
@@ -23,8 +23,10 @@ define([
 			
 			this.$el.html(this.template(this.model.toJSON()));
 			
-			// render star value
+			// render hotel star value
+			// TODO: replace path to star images for this instance to a copy that has a small drop shadow to help with visibility when positioned over hotel thumbnail images
 			$('.star-value', this.$el).raty({
+				hints: [l('1_STAR_HOTEL'), l('2_STAR_HOTEL'), l('3_STAR_HOTEL'), l('4_STAR_HOTEL'), l('5_STAR_HOTEL')],
 				path: 'lib/raty/images',
 				readOnly: true,
 				score: this.model.get('stars')
