96f01ac5d054da4b97b50783547e2f7a2548dadc
Jerome Dane <jerome.dane@gmail.com>
2015-07-09 10:25:39 -0400

    Use uglify2 for tiny build size improvement

--- public/build.js
+++ public/build.js
@@ -3,6 +3,7 @@
 	appDir: './',
 	removeCombined: true,
 	optimizeCss: 'standard',
+	optimize: 'uglify2',
     dir:'./dist',
 	wrapShim: true,
 	fileExclusionRegExp: /^(\..+|docs|(r|build)\.(js|bat)|yuidoc\.json|.+\.(zip|bat|map)|nbproject)$/,
