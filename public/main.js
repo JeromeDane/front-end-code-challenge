d917ab10e28405b9720f169bd638c594202fb79f
Jerome Dane <jerome.dane@gmail.com>
2015-07-09 10:39:35 -0400

    Move require configuration into config.js so that it can be used both in development and build process

--- public/main.js
+++ public/main.js
@@ -12,101 +12,12 @@ var IGNORE_CACHE = true;
 
 // set up require.js configuration
 require.config({
-	
 	// cache busting
-	urlArgs: IGNORE_CACHE ? 'nocache=' +  (new Date()).getTime() : '',
-	
-	// paths to libraries
-	paths: {
-		'backbone': 'lib/backbone/backbone.min',
-		'css': 'lib/require/css',
-		'jquery': 'lib/jquery/jquery.min',
-		'jquery-ui': 'lib/jquery-ui/jquery-ui.min',
-		'json': 'lib/require/json',
-		'l10n': 'lib/l10n',
-		'localization': 'lib/jeromedane/localization',
-		'masonry': 'lib/masonry/masonry.min',
-		'jerome-utils': 'lib/jeromedane/utils',
-		'raty': 'lib/raty/jquery.raty',
-		'sly': 'lib/sly/sly.min',
-		'swipebox': 'lib/swipebox/js/jquery.swipebox.min',
-		'text': 'lib/require/text',
-		'tooltipster': 'lib/tooltipster/jquery.tooltipster.min',
-		'touchpunch': 'lib/touchpunch/touchpunch.min',
-		'ui': 'views/ui',
-		'underscore': 'lib/underscore/underscore.min'
-	},
-	
-	// dependencies
-    shim: {
-		'backbone': {
-			exports: 'Backbone',
-			deps: ['underscore']
-		},
-    	'jquery': {
-    		exports: 'jQuery'
-    	},
-		'jquery-ui': {
-			deps: [
-				'jquery',
-				'css!lib/jquery-ui/jquery-ui.min'
-			]
-    	},
-		'masonry': {
-			deps: ['jquery']
-		},
-		'localization': {
-			deps: ['l10n']
-		},
-		'raty': {
-			deps: [
-				'jquery',
-				'css!lib/raty/jquery.raty'
-			]
-		},
-		'sly': {
-			expords: 'Sly',
-			deps: [
-				'jquery'
-			]
-		},
-		'swipebox': {
-			deps: [
-				'jquery',
-				'css!lib/swipebox/css/swipebox.min'
-			]
-		},
-		'tooltipster': {
-			deps: [
-				'jquery',
-				'css!lib/tooltipster/css/tooltipster',
-				'css!lib/tooltipster/css/themes/tooltipster-shadow'
-			]
-		},
-		'touchpunch': {
-			deps: ['jquery-ui']
-		},
-		'ui': {
-			deps: [
-				'backbone',
-				'css!styles/ui',
-				'jquery-ui',
-				'localization',
-				'jerome-utils',
-				'raty',
-				'sly',
-				'swipebox',
-				'tooltipster',
-				'touchpunch'
-			]
-		},
-		'underscore': {
-			exports: '_'
-		}
-    }
+	urlArgs: IGNORE_CACHE ? 'nocache=' +  (new Date()).getTime() : ''
 });
 
 // load and render the main UI to the body element of the HTML page
+// TODO: Figure out why require.js optimizer doesn't understand the ui deps defined in config.js shim
 require([
 	'backbone',
 	'css!styles/ui',
